ffd331ea21ffe54f7a5ff4df1afb990c53bf3cee
Ettore <ettorecelozzi@gmail.com>
2018-06-21 17:13:53 +0200

    Delete improvements.js

--- website/inailmalattie/scripts/improvements.js
+++
@@ -1,8 +0,0 @@
-//JavaScript Document
-document.addEventListener('DOMContentLoaded',function(){
-	console.log('start');
-	contacts = document.getElementById('listacontatti');
-	contacts.addEventListener('click',function(){
-		contacts.style.display = 'block';
-	})
-})
