c5d40145d45b877d9d5b71a7b3e9171ea35df5d1
ciabo <lucaciabo@hotmail.it>
2018-06-23 18:29:37 +0200

    Add files via upload

---
+++ website/inailmalattie/server/handler.php
@@ -0,0 +1,70 @@
+<?php
+	header('Content-Type: text/json');
+	require_once("config.php");
+	$action = $_POST['action'];
+
+	switch($action) {
+		
+		case "load" : 
+			loadData();
+		break;
+		case "filter" :
+			filterData();
+		break; 		
+	}
+	
+	function loadData() {
+		//$query = 'SELECT singolesedi.latitudineProvincia, singolesedi.longitudineProvincia FROM datisemestrali JOIN singolesedi on datisemestrali.sedeInail=singolesedi.codice'; 
+		$query = 'SELECT singolesedi.latitudineProvincia, singolesedi.longitudineProvincia, count(*) AS count FROM datisemestrali JOIN singolesedi on datisemestrali.sedeInail=singolesedi.codice group by singolesedi.latitudineProvincia, singolesedi.longitudineProvincia';
+		$mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE); 
+		$result = $mysqli->query($query_string); 
+    	$data = array();	
+		while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
+		
+			$latitudine = $row['latitudineProvincia'];
+			$longitudine = $row['longitudineProvincia'];
+			$count = $row['count'];
+  
+			$dataElement = array('latitudine' => $latitudine,'longitudine' =>$longitudine, 'count' => $count);
+			array_push($data, $dataElement);
+		}
+	
+    	$response = array('data' => $data, 'type' => 'load');
+		echo json_encode($response);	
+	
+}
+	function filterData(){
+		$tumore=$_POST['tumore']; //tumore può essere 1 (se il filtro è attivo) oppure 0
+		$amianto=$_POST['amianto']; //amianto può essere 1 (se il filtro è attivo) oppure 0
+		$genere=$_POST['genere'];//genere può essere 1 (se cerchiamo maschi), 2 (se cerchiamo donne) oppure 0 se se il filtro non è attivo
+		$dataInizio=$_POST['dataInizio'];//una data oppure 0
+		$dataFine=$_POST['dataFine'];//una data oppure 0
+		$morto=$_POST['morto']; //morto può essere 1 (se il filtro è attivo) oppure zero
+		
+		$query_string = "SELECT singolesedi.latitudineProvincia, singolesedi.longitudineProvincia, count(*) AS count 
+		FROM datisemestrali JOIN singolesedi on datisemestrali.sedeInail=singolesedi.codice ".
+		($tumore==1) ? ."JOIN tumori on datisemestrali.ICD10denunciato=tumori.id". : ."".."
+		WHERE ". ($morto==1) ? ." datisemestrali.dataMorte is not NULL". : ."datisemestrali.dataMorte is not NULL or datisemestrali.dataMorte is NULL". ."
+		".($amianto==1) ? ."AND datisemestrali.asbestoCorrelata=1". : ."". ."
+		". ($dataInizio!=0) ? ."AND datisemestrali.data>\'".$dataInizio."\' AND datisemestrali.data<\'".$dataFine."\'". : ."". ."
+		GROUP BY singolesedi.latitudineProvincia, singolesedi.longitudineProvincia'";
+		
+		$mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE); 
+		$result = $mysqli->query($query_string); 
+    	$data = array();	
+		while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
+		
+			$latitudine = $row['latitudineProvincia'];
+			$longitudine = $row['longitudineProvincia'];
+			$count = $row['count'];
+  
+			$dataElement = array('latitudine' => $latitudine,'longitudine' =>$longitudine, 'count' => $count);
+			array_push($data, $dataElement);
+		}
+	
+    	$response = array('data' => $data, 'type' => 'load');
+		echo json_encode($response);	
+	}
+	
+	
+?>
