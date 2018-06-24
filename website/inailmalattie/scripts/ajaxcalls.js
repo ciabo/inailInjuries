2f7d5a2beb38c140af80306853212eaf0d870d4c
Ettore <ettorecelozzi@gmail.com>
2018-06-24 18:01:19 +0200

    Add files via upload

--- website/inailmalattie/scripts/ajaxcalls.js
+++ website/inailmalattie/scripts/ajaxcalls.js
@@ -1,7 +1,7 @@
 // JavaScript Document
+addressPoints = [];
 
 (function($){
-	console.log("JQUERY: " + $);
 	
 	
 		serverURL = "server/handler.php" 
@@ -9,6 +9,7 @@
 		//AGGIUNGERE I GESTORI DI EVENTI SUI PULSANTI !!
 			
 		filterData();
+		loadData();
 			
 		 function loadData() {
 				console.log("loadData");
@@ -18,9 +19,9 @@
 					url: serverURL,
 					type: "POST",
 					//contentType: 'application/json; charset=utf-8',
-					data: {"action" : request_type},
+					data: {"action" : request_type}, //invio la richiesta al server
 					dataType: "json",
-					success:function(data){
+					success:function(data){ //data contiene la risposta dal server
 						handleLoad(data);
 					},
 					error: function(XHR, err) {
@@ -36,7 +37,27 @@
 			function handleLoad(data) {
 				console.log("handleLoad");
 				//data a questo punto è un array che contiene 106 elementi ognuno con latitudine, longitudine e count. 
+				/*console.log(data[0]);
+				var a = data[0]; 
+				var lat = a.latitudine;
+				var lng = a.longitudine
+				var count = a.count;
+				console.log(" " + lat + lng + count);*/
+				
+				var markers = L.markerClusterGroup({ chunkedLoading: true }); //creo il cluster
+				for (var i = 0; i < data.length; i++) {
+					var a = data[i]; 
+					var lat = a.latitudine;
+					var lng = a.longitudine
+					var count = a.count;
+					var marker = L.marker(L.latLng(lat, lng), { count: count }); //creo marker con coordinate del vettore
+					marker.bindPopup(count); //metodo per far apparire il titolino sul cursore
+					markers.addLayer(marker); //aggiungo il marcatore
+				}
+
+				map.addLayer(markers); // aggiungo I marcatori
 			}
+			
 			
 			function filterData(){ //questa funzione verrò chiamata ogni volta che si varia un valore nel filtro 
 				console.log("filterData");
@@ -62,9 +83,29 @@
 			}
 			
 			function handleFilter(data){
-				console.log("handleLoad");
-				//data a questo punto è un array che contiene elementi ognuno con latitudine, longitudine, settore e count.
-				//bisogna quindi gestire il fatto del settore: se il filtro è attivato si fa apparire di colore diverso
-				//i vari settori, altrimenti tutto normale
+				console.log("handleLoadF");
+				//data a questo punto è un array che contiene elementi ognuno con latitudine, longitudine e count. 
 			}
 })(jQuery);
+
+
+latlng = L.latLng(42.56, 12.50);
+var map = L.map('map', {center: latlng, zoom: 13});
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.streets',
+    accessToken: 'pk.eyJ1IjoiOWV0dG9yZTYiLCJhIjoiY2ppb3FpOXlxMGM5ODN2dDl6Mjh2cTUwOCJ9.QoSRENS3F5CVJSIjLywTxg'
+}).addTo(map);
+/*
+console.log(addressPoints);
+var markers = L.markerClusterGroup({ chunkedLoading: true }); //creo il cluster
+for (var i = 0; i < addressPoints.length; i++) {
+	var a = addressPoints[i];
+	var title = a[2];
+	var marker = L.marker(L.latLng(a[0], a[1]), { title: title }); //creo marker con coordinate del vettore
+	marker.bindPopup(title); //metodo per far apparire il titolino sul cursore
+	markers.addLayer(marker); //aggiungo il marcatore
+}
+
+map.addLayer(markers); // aggiungo I marcatori */
